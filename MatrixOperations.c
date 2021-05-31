d5c75f32adcdd536980957894591a8abc120d359
stanleysie <stanleylawrencesie@gmail.com>
2021-05-31 15:42:57 +0800

    working vector-vector and vector-scalar addition and subtraction

--- MatrixOperations.c
+++ MatrixOperations.c
@@ -3,84 +3,66 @@
 #include <string.h>
 #include "MatrixOperations.h"
 
-void vector_vector_add(int col, v512P A, v512P B, v512P C) {
+void vector_vector_add(int n, v512P A, v512P B, v512P C) {
 	asm(
-		"l1:					\n"
 		"vsetvli	t0, %0, e32	\n"
 		"vlw.v		v0, (%1)	\n"
-		"sub		%0, %0, t0	\n"
-        "slli		t0, t0, 2	\n"
-        "add		%1, %1, t0	\n"
 		"vlw.v		v1, (%2)	\n"
-		"add		%2, %2, t0	\n"
 		"vfadd.vv	v2, v0, v1	\n"
 		"vsw.v		v2, (%3)	\n"
-		"add		%3, %3, t0	\n"
-        "bnez    	%0, l1		\n"
 		:
-		: "r"(col), "r"(A), "r"(B), "r"(C)
+		: "r"(n), "r"(A), "r"(B), "r"(C)
 	);
 }
 
-void vector_vector_sub(int col, v512P A, v512P B, v512P C) {
+void vector_vector_sub(int n, v512P A, v512P B, v512P C) {
 	asm(
-		"l2:					\n"
 		"vsetvli	t0, %0, e32	\n"
 		"vlw.v		v0, (%1)	\n"
-		"sub		%0, %0, t0	\n"
-        "slli		t0, t0, 2	\n"
-        "add		%1, %1, t0	\n"
 		"vlw.v		v1, (%2)	\n"
-		"add		%2, %2, t0	\n"
 		"vfsub.vv	v2, v0, v1	\n"
 		"vsw.v		v2, (%3)	\n"
-		"add		%3, %3, t0	\n"
-        "bnez    	%0, l2		\n"
 		:
-		: "r"(col), "r"(A), "r"(B), "r"(C)
+		: "r"(n), "r"(A), "r"(B), "r"(C)
 	);
 }
 
-//void vector_scalar_add(int col, v512P A, float num, v512P C) {
-//	asm(
-//		"l3:					\n"
-//		"vsetvli	t0, %0, e32	\n"
-//		"vlw.v		v0, (%1)	\n"
-//		"sub		%0, %0, t0	\n"
-//        "slli		t0, t0, 2	\n"
-//        "add		%1, %1, t0	\n"
-//		"vfadd.vx	v1, v0, %2	\n"
-//		"vsw.v		v1, (%3)	\n"
-//		"add		%3, %3, t0	\n"
-//        "bnez    	%0, l3		\n"
-//		:
-//		: "r"(col), "r"(A), "r"(num), "r"(C)
-//	);
-//}
-//
-//void vector_scalar_sub(int col, v512P A, float num, v512P C) {
-//	asm(
-//		"l4:					\n"
-//		"vsetvli	t0, %0, e32	\n"
-//		"vlw.v		v0, (%1)	\n"
-//		"sub		%0, %0, t0	\n"
-//        "slli		t0, t0, 2	\n"
-//        "add		%1, %1, t0	\n"
-//		"vfsub.vx	v1, v0, %2	\n"
-//		"vsw.v		v1, (%3)	\n"
-//		"add		%3, %3, t0	\n"
-//        "bnez    	%0, l4		\n"
-//		:
-//		: "r"(col), "r"(A), "r"(num), "r"(C)
-//	);
-//}
+void vector_scalar_add(int n, v512P A, float num, v512P C) {
+	asm(
+		"vsetvli	t0, %0, e32	\n"
+		"vlw.v		v0, (%1)	\n"
+		"flw		f0, 0(%2)	\n"
+		"vfadd.vf	v1, v0, f0	\n"
+		"vsw.v		v1, (%3)	\n"
+		:
+		: "r"(n), "r"(A), "r"(&num), "r"(C)
+	);
+}
+
+void vector_scalar_sub(int n, v512P A, float num, v512P C) {
+	asm(
+		"vsetvli	t0, %0, e32	\n"
+		"vlw.v		v0, (%1)	\n"
+		"flw		f0, 0(%2)	\n"
+		"vfsub.vf	v1, v0, f0	\n"
+		"vsw.v		v1, (%3)	\n"
+		:
+		: "r"(n), "r"(A), "r"(&num), "r"(C)
+	);
+}
 
 int
 main() {
 	enableVEC();
 	enableFP();
 	
-	int row = 25, col = 25;
+	int row = 100, col = 16;
+	float num = 100.5;
+	if(col > VEC32) {
+		printf("[Error] - number of columns exceed the capacity of registers.\n\n");
+		return 1;
+	}
+	
 	int i, j;
 	
 	float A[row][col];
@@ -91,33 +73,30 @@ main() {
 	for(i = 0; i < row; i++) {
 		for(j = 0; j < col; j++) {
 			A[i][j] = i + j;
-			B[i][j] = i + j;
+			B[i][j] = i;
 		}
 	}
 	
 	for(i = 0; i < row; i++) {
-		mA.el[i] = malloc(sizeof(v512T));
-		mB.el[i] = malloc(sizeof(v512T));
-		mC.el[i] = malloc(sizeof(v512T));
+		mA.mem[i] = malloc(sizeof(v512T));
+		mB.mem[i] = malloc(sizeof(v512T));
+		mC.mem[i] = malloc(sizeof(v512T));
+		
 		for(j = 0; j < col; j++) {
-			mA.el[i]->vf32[j] = A[i][j];
-			mB.el[i]->vf32[j] = B[i][j];
+			mA.mem[i]->vf32[j] = A[i][j];
+			mB.mem[i]->vf32[j] = B[i][j];
 		}
-		vector_vector_add(col, mA.el[i], mB.el[i], mC.el[i]);
-	}
-	
-	// check
-	for(i = 0; i < row; i++) {
-		for(j = 0; j < col; j++) {
-			printf("[%d,%d] %f %f %f \n", i, j, mA.el[i]->vf32[j], mB.el[i]->vf32[j], mC.el[i]->vf32[j]);
-		}
+		
+		// call functions here
+		vector_vector_add(col, mA.mem[i], mB.mem[i], mC.mem[i]);
+//		vector_scalar_add(col, mA.mem[i], num, mC.mem[i]);
 	}
 	
 	// free allocated memory
 	for(i = 0; i < row; i++) {
-		free(mA.el[i]);
-		free(mB.el[i]);
-		free(mC.el[i]);
+		free(mA.mem[i]);
+		free(mB.mem[i]);
+		free(mC.mem[i]);
 	}
 	
 	return 0;
--- MatrixOperations.h
+++ MatrixOperations.h
@@ -2,10 +2,10 @@
 #define MAX_SIZE 1024
 
 typedef struct {
-	v512P el[MAX_SIZE];
+	v512P mem[MAX_SIZE];
 } MATRIX;
 
-void vector_vector_add(int col, v512P A, v512P B, v512P C);
-void vector_vector_sub(int col, v512P A, v512P B, v512P C);
-//void vector_scalar_add(int col, v512P A, float num, v512P C);
-//void vector_scalar_sub(int col, v512P A, float num, v512P C);
+void vector_vector_add(int n, v512P A, v512P B, v512P C);
+void vector_vector_sub(int n, v512P A, v512P B, v512P C);
+void vector_scalar_add(int n, v512P A, float num, v512P C);
+void vector_scalar_sub(int n, v512P A, float num, v512P C);
