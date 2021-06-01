cd119ee64d9c1a74a935e7c61c6749cc41849210
stanleysie <stanleylawrencesie@gmail.com>
2021-06-02 03:02:50 +0800

    Working matrix operations with sample linear regression

--- MatrixOperations.c
+++ MatrixOperations.c
@@ -1,80 +1,312 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "vsupport.h"
 #include "MatrixOperations.h"
 
-void matrix_matrix_add(int row, int col, MATRIX A, MATRIX B, MATRIX C) {
+#define DATA_SIZE 30
+
+typedef struct {
+	float yearsExperience;
+	float salary;
+} DATA;
+
+void vector_vector_add(int col, float A[], float B[], float C[]) {
+	asm(
+		"vvadd:						\n"
+		"	vsetvli		t0, %0, e32	\n"
+		"	vlw.v		v0, (%1)	\n"
+		"	sub			%0, %0, t0	\n"
+		"	slli		t0, t0, 2	\n"
+		"	add			%1, %1, t0	\n"
+		"	vlw.v		v1, (%2)	\n"
+		"	add			%2, %2, t0	\n"
+		"	vfadd.vv	v2, v0, v1	\n"
+		"	vsw.v		v2, (%3)	\n"
+		"	add			%3, %3, t0	\n"
+		"	bnez		%0, vvadd	\n"
+		:
+		: "r"(col), "r"(&A[0]), "r"(&B[0]), "r"(&C[0])
+	);	
+}
+
+void vector_vector_sub(int col, float A[], float B[], float C[]) {
+	asm(
+		"vvsub:						\n"
+		"	vsetvli		t0, %0, e32	\n"
+		"	vlw.v		v0, (%1)	\n"
+		"	sub			%0, %0, t0	\n"
+		"	slli		t0, t0, 2	\n"
+		"	add			%1, %1, t0	\n"
+		"	vlw.v		v1, (%2)	\n"
+		"	add			%2, %2, t0	\n"
+		"	vfsub.vv	v2, v0, v1	\n"
+		"	vsw.v		v2, (%3)	\n"
+		"	add			%3, %3, t0	\n"
+		"	bnez		%0, vvsub	\n"
+		:
+		: "r"(col), "r"(&A[0]), "r"(&B[0]), "r"(&C[0])
+	);	
+}
+
+void vector_vector_mul(int col, float A[], float B[], float C[]) {
+	asm(
+		"vvmul:						\n"
+		"	vsetvli		t0, %0, e32	\n"
+		"	vlw.v		v0, (%1)	\n"
+		"	sub			%0, %0, t0	\n"
+		"	slli		t0, t0, 2	\n"
+		"	add			%1, %1, t0	\n"
+		"	vlw.v		v1, (%2)	\n"
+		"	add			%2, %2, t0	\n"
+		"	vfmul.vv	v2, v0, v1	\n"
+		"	vsw.v		v2, (%3)	\n"
+		"	add			%3, %3, t0	\n"
+		"	bnez		%0, vvmul	\n"
+		:
+		: "r"(col), "r"(&A[0]), "r"(&B[0]), "r"(&C[0])
+	);	
+}
+
+void vector_scalar_add(int col, float A[], float num, float C[]) {
+	asm(
+		"vsadd:						\n"
+		"	vsetvli		t0, %0, e32	\n"
+		"	vlw.v		v0, (%1)	\n"
+		"	sub			%0, %0, t0	\n"
+		"	slli		t0, t0, 2	\n"
+		"	add			%1, %1, t0	\n"
+		"	flw			f0, 0(%2)	\n"
+		"	vfadd.vf	v1, v0, f0	\n"
+		"	vsw.v		v1, (%3)	\n"
+		"	add			%3, %3, t0	\n"
+		"	bnez		%0, vsadd	\n"
+		:
+		: "r"(col), "r"(&A[0]), "r"(&num), "r"(&C[0])
+	);
+}
+
+void vector_scalar_sub(int col, float A[], float num, float C[]) {
+	asm(
+		"vssub:						\n"
+		"	vsetvli		t0, %0, e32	\n"
+		"	vlw.v		v0, (%1)	\n"
+		"	sub			%0, %0, t0	\n"
+		"	slli		t0, t0, 2	\n"
+		"	add			%1, %1, t0	\n"
+		"	flw			f0, 0(%2)	\n"
+		"	vfsub.vf	v1, v0, f0	\n"
+		"	vsw.v		v1, (%3)	\n"
+		"	add			%3, %3, t0	\n"
+		"	bnez		%0, vssub	\n"
+		:
+		: "r"(col), "r"(&A[0]), "r"(&num), "r"(&C[0])
+	);
+}
+
+void vector_scalar_mul(int col, float A[], float num, float C[]) {
+	asm(
+		"vsmul:						\n"
+		"	vsetvli		t0, %0, e32	\n"
+		"	vlw.v		v0, (%1)	\n"
+		"	sub			%0, %0, t0	\n"
+		"	slli		t0, t0, 2	\n"
+		"	add			%1, %1, t0	\n"
+		"	flw			f0, 0(%2)	\n"
+		"	vfmul.vf	v1, v0, f0	\n"
+		"	vsw.v		v1, (%3)	\n"
+		"	add			%3, %3, t0	\n"
+		"	bnez		%0, vsmul	\n"
+		:
+		: "r"(col), "r"(&A[0]), "r"(&num), "r"(&C[0])
+	);
+}
+
+void matrix_matrix_add(int row, int col, float A[][col], float B[][col], float C[][col]) {
 	int i;
 	for(i = 0; i < row; i++) {
 		asm(
-			"vsetvli	t0, %0, e32	\n"
-			"vlw.v		v0, (%1)	\n"
-			"vlw.v		v1, (%2)	\n"
-			"vfadd.vv	v2, v0, v1	\n"
-			"vsw.v		v2, (%3)	\n"
+			"lvadd:						\n"
+			"	vsetvli		t0, %0, e32	\n"
+			"	vlw.v		v0, (%1)	\n"
+			"	sub			%0, %0, t0	\n"
+			"	slli		t0, t0, 2	\n"
+			"	add			%1, %1, t0	\n"
+			"	vlw.v		v1, (%2)	\n"
+			"	add			%2, %2, t0	\n"
+			"	vfadd.vv	v2, v0, v1	\n"
+			"	vsw.v		v2, (%3)	\n"
+			"	add			%3, %3, t0	\n"
+			"	bnez		%0, lvadd	\n"
 			:
-			: "r"(col), "r"(A.mem[i]), "r"(B.mem[i]), "r"(C.mem[i])
+			: "r"(col), "r"(&A[i][0]), "r"(&B[i][0]), "r"(&C[i][0])
 		);	
 	}
 }
 
-void matrix_matrix_sub(int row, int col, MATRIX A, MATRIX B, MATRIX C) {
+void matrix_matrix_sub(int row, int col, float A[][col], float B[][col], float C[][col]) {
 	int i;
 	for(i = 0; i < row; i++) {
 		asm(
-			"vsetvli	t0, %0, e32	\n"
-			"vlw.v		v0, (%1)	\n"
-			"vlw.v		v1, (%2)	\n"
-			"vfsub.vv	v2, v0, v1	\n"
-			"vsw.v		v2, (%3)	\n"
+			"lvsub:						\n"
+			"	vsetvli		t0, %0, e32	\n"
+			"	vlw.v		v0, (%1)	\n"
+			"	sub			%0, %0, t0	\n"
+			"	slli		t0, t0, 2	\n"
+			"	add			%1, %1, t0	\n"
+			"	vlw.v		v1, (%2)	\n"
+			"	add			%2, %2, t0	\n"
+			"	vfsub.vv	v2, v0, v1	\n"
+			"	vsw.v		v2, (%3)	\n"
+			"	add			%3, %3, t0	\n"
+			"	bnez		%0, lvsub	\n"
 			:
-			: "r"(col), "r"(A.mem[i]), "r"(B.mem[i]), "r"(C.mem[i])
+			: "r"(col), "r"(&A[i][0]), "r"(&B[i][0]), "r"(&C[i][0])
 		);	
 	}
 }
 
-void matrix_scalar_add(int row, int col, MATRIX A, float num, MATRIX C) {
+void matrix_matrix_mul(int row, int col, int len, float A[][len], float B[][col], float C[][col]) {
+	float tempC[row][col][len];
+	
+	int i, j, k;
+	for(i = 0; i < row; i++) {
+		for(j = 0; j < col; j++) {
+			asm(
+				"lvmul:						\n"
+				"	vsetvli		t0, %0, e32	\n"
+				"	vlw.v		v0, (%1)	\n"
+				"	sub			%0, %0, t0	\n"
+				"	slli		t0, t0, 2	\n"
+				"	add			%1, %1, t0	\n"
+				"	vlsw.v		v1, (%2), %4\n"
+				"	add			%2, %2, t0	\n"
+				"	vfmul.vv	v2, v0, v1	\n"
+				"	vsw.v		v2, (%3)	\n"
+				"	add			%3, %3, t0	\n"
+				"	bnez		%0, lvmul	\n"
+				:
+				: "r"(len), "r"(&A[i][0]), "r"(&B[0][j]), "r"(&tempC[i][j][0]), "r"(col*4)
+			);		
+		}
+	}
+	
+	for(i = 0; i < row; i++) {
+		for(j = 0; j < col; j++) {
+			float sum = 0;
+			for(k = 0; k < col; k++) {
+				sum += tempC[i][j][k];
+			}
+			C[i][j] = sum;
+		}
+	}
+}
+
+void matrix_scalar_add(int row, int col, float A[][col], float num, float C[][col]) {
 	int i;
 	for(i = 0; i < row; i++) {
 		asm(
-			"vsetvli	t0, %0, e32	\n"
-			"vlw.v		v0, (%1)	\n"
-			"flw		f0, 0(%2)	\n"
-			"vfadd.vf	v1, v0, f0	\n"
-			"vsw.v		v1, (%3)	\n"
+			"lsadd:						\n"
+			"	vsetvli		t0, %0, e32	\n"
+			"	vlw.v		v0, (%1)	\n"
+			"	sub			%0, %0, t0	\n"
+			"	slli		t0, t0, 2	\n"
+			"	add			%1, %1, t0	\n"
+			"	flw			f0, 0(%2)	\n"
+			"	vfadd.vf	v1, v0, f0	\n"
+			"	vsw.v		v1, (%3)	\n"
+			"	add			%3, %3, t0	\n"
+			"	bnez		%0, lsadd	\n"
 			:
-			: "r"(col), "r"(A.mem[i]), "r"(&num), "r"(C.mem[i])
+			: "r"(col), "r"(&A[i][0]), "r"(&num), "r"(&C[i][0])
 		);
 	}
 }
 
-void matrix_scalar_sub(int row, int col, MATRIX A, float num, MATRIX C) {
+void matrix_scalar_sub(int row, int col, float A[][col], float num, float C[][col]) {
 	int i;
 	for(i = 0; i < row; i++) {
 		asm(
-			"vsetvli	t0, %0, e32	\n"
-			"vlw.v		v0, (%1)	\n"
-			"flw		f0, 0(%2)	\n"
-			"vfsub.vf	v1, v0, f0	\n"
-			"vsw.v		v1, (%3)	\n"
+			"lssub:						\n"
+			"	vsetvli		t0, %0, e32	\n"
+			"	vlw.v		v0, (%1)	\n"
+			"	sub			%0, %0, t0	\n"
+			"	slli		t0, t0, 2	\n"
+			"	add			%1, %1, t0	\n"
+			"	flw			f0, 0(%2)	\n"
+			"	vfsub.vf	v1, v0, f0	\n"
+			"	vsw.v		v1, (%3)	\n"
+			"	add			%3, %3, t0	\n"
+			"	bnez		%0, lssub	\n"
 			:
-			: "r"(col), "r"(A.mem[i]), "r"(&num), "r"(C.mem[i])
+			: "r"(col), "r"(&A[i][0]), "r"(&num), "r"(&C[i][0])
 		);
 	}
 }
 
-void matrix_scalar_mul(int row, int col, MATRIX A, float num, MATRIX C) {
+void matrix_scalar_mul(int row, int col, float A[][col], float num, float C[][col]) {
 	int i;
 	for(i = 0; i < row; i++) {
 		asm(
-			"vsetvli	t0, %0, e32	\n"
-			"vlw.v		v0, (%1)	\n"
-			"flw		f0, 0(%2)	\n"
-			"vfmul.vf	v1, v0, f0	\n"
-			"vsw.v		v1, (%3)	\n"
+			"lsmul:						\n"
+			"	vsetvli		t0, %0, e32	\n"
+			"	vlw.v		v0, (%1)	\n"
+			"	sub			%0, %0, t0	\n"
+			"	slli		t0, t0, 2	\n"
+			"	add			%1, %1, t0	\n"
+			"	flw			f0, 0(%2)	\n"
+			"	vfmul.vf	v1, v0, f0	\n"
+			"	vsw.v		v1, (%3)	\n"
+			"	add			%3, %3, t0	\n"
+			"	bnez		%0, lsmul	\n"
 			:
-			: "r"(col), "r"(A.mem[i]), "r"(&num), "r"(C.mem[i])
+			: "r"(col), "r"(&A[i][0]), "r"(&num), "r"(&C[i][0])
 		);
+	}
+}
+
+void print_matrix(int row, int col, float M[][col], char s[]) {
+	int i, j;
+	printf("\n%s\n", s);
+	for(i = 0; i < row; i++) {
+		for(j = 0; j < col; j++) {
+			printf("%.2f\t", M[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void load_data(DATA *dataset) {
+	FILE *fp;
+	
+	fp = fopen("linear_dataset.csv", "r");
+	if (fp == NULL) {
+		printf("Error in reading file.\n");
+	} else {
+		char line[1024];
+		int row = 0, column = 0;
+		
+		while(fgets(line, sizeof(line), fp)) {
+			column = 0;
+	        row++;
+	        
+			if(row == 0) continue;
+		
+			char *data = strtok(line, ",");
+			
+			while(data) {
+				if(column == 0) {
+					dataset[row - 2].yearsExperience = atof(data);
+				} else if(column == 1) {
+					dataset[row - 2].salary = atof(data);
+				}
+				data = strtok(NULL, ", ");
+				column++;
+			}
+		}
+		
+		fclose(fp);
 	}
 }
 
@@ -83,48 +315,36 @@ main() {
 	enableVEC();
 	enableFP();
 	
-	int row = 100, col = 16;
-//	float num = 100.0;
-	if(col > VEC32) {
-		printf("[Error] - number of columns exceeded the capacity of registers.\n\n");
-		return 1;
+	DATA dataset[DATA_SIZE];
+	load_data(dataset);
+	
+	float X[DATA_SIZE];
+	float Y[DATA_SIZE];
+	float XX[DATA_SIZE];
+	float XY[DATA_SIZE];
+	
+	int i;
+	for(i = 0; i < DATA_SIZE; i++) {
+		X[i] = dataset[i].yearsExperience;
+		Y[i] = dataset[i].salary;
 	}
 	
-	int i, j;
+	vector_vector_mul(DATA_SIZE, X, X, XX);
+	vector_vector_mul(DATA_SIZE, X, Y, XY);
 	
-	float A[row][col];
-	float B[row][col];
-	
-	MATRIX mA, mB, mC;
-	
-	for(i = 0; i < row; i++) {
-		for(j = 0; j < col; j++) {
-			A[i][j] = i + j;
-			B[i][j] = i;
-		}
+	float sumX = 0, sumY = 0, sumXX = 0, sumXY = 0;
+	for(i = 0; i < DATA_SIZE; i++) {
+		sumX += X[i];
+		sumY += Y[i];
+		sumXX += XX[i];
+		sumXY += XY[i];
 	}
 	
-	for(i = 0; i < row; i++) {
-		mA.mem[i] = malloc(sizeof(v512T));
-		mB.mem[i] = malloc(sizeof(v512T));
-		mC.mem[i] = malloc(sizeof(v512T));
-		
-		for(j = 0; j < col; j++) {
-			mA.mem[i]->vf32[j] = A[i][j];
-			mB.mem[i]->vf32[j] = B[i][j];
-		}
-	}
+	float b = (DATA_SIZE*sumXY - sumX*sumY) / (DATA_SIZE*sumXX - sumX*sumX);
+	float a = (sumY - b*sumX) / DATA_SIZE;
 	
-	// call functions here
-	matrix_matrix_add(row, col, mA, mB, mC);
-//	matrix_scalar_add(row, col, mA, num, mC);
-	
-	// free allocated memory
-	for(i = 0; i < row; i++) {
-		free(mA.mem[i]);
-		free(mB.mem[i]);
-		free(mC.mem[i]);
-	}
+	printf("\nValues are: a = %0.2f and b = %0.2f\n",a,b);
+ 	printf("Equation of best fit is: y = %0.2f + %0.2fx\n\n",a,b);
 	
 	return 0;
 }
--- linear/run_linear.sh
+++
@@ -1,1 +0,0 @@
-riscvOVPsimPlus.exe --variant RV64GCV --program v-linear-regression.elf --override riscvOVPsim/cpu/vector_version=0.8
